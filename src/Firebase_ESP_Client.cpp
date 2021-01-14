a5176de94b399dae728b5187db05750ebc8c9ce0
suwatchai <k_suwatchai@hotmail.com>
2021-01-14 12:23:49 +0700

    Minor fix header inclusion

--- src/Firebase_ESP_Client.cpp
+++ src/Firebase_ESP_Client.cpp
@@ -1,15 +1,15 @@
 /**
- * Google's Firebase ESP Client Main class, Firebase_ESP_Client.cpp version 1.0.0
+ * Google's Firebase ESP Client Main class, Firebase_ESP_Client.cpp version 1.0.1
  * 
  * This library supports Espressif ESP8266 and ESP32
  * 
- * Created January 12, 2021
+ * Created January 14, 2021
  * 
  * This work is a part of Firebase ESP Client library
- * Copyright (c) 2021, 2021 K. Suwatchai (Mobizt)
+ * Copyright (c) 2020, 2021 K. Suwatchai (Mobizt)
  * 
  * The MIT License (MIT)
- * Copyright (c) 2021, 2021 K. Suwatchai (Mobizt)
+ * Copyright (c) 2020, 2021 K. Suwatchai (Mobizt)
  * 
  * 
  * Permission is hereby granted, free of charge, to any person returning a copy of
