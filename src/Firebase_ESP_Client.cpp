8adf46a1a865f6a9800346a3216044205c6d227f
suwatchai <k_suwatchai@hotmail.com>
2021-01-30 21:41:42 +0700

    Urgently fixed for missing sources file during publish the update.

--- src/Firebase_ESP_Client.cpp
+++ src/Firebase_ESP_Client.cpp
@@ -3,7 +3,7 @@
  * 
  * This library supports Espressif ESP8266 and ESP32
  * 
- * Created January 29, 2021
+ * Created January 30, 2021
  * 
  * This work is a part of Firebase ESP Client library
  * Copyright (c) 2020, 2021 K. Suwatchai (Mobizt)
