cb7ea94c6ecb0554929539cbf68efed185d1fbcc
suwatchai <k_suwatchai@hotmail.com>
2021-10-25 17:30:57 +0700

    Update ESP32 ethernet examples for WiFi event enums changed in Core v2.0.0.

--- examples/RTDB/BasicEthernet/ESP32/ESP32.ino
+++ examples/RTDB/BasicEthernet/ESP32/ESP32.ino
@@ -103,6 +103,46 @@ bool firebaseConfigReady = false;
 void WiFiEvent(WiFiEvent_t event)
 {
     //Do not run any function here to prevent stack overflow or nested interrupt
+
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 4, 0)
+
+    switch (event)
+    {
+    case ARDUINO_EVENT_ETH_START:
+        Serial.println("ETH Started");
+        //set eth hostname here
+        ETH.setHostname("esp32-ethernet");
+        break;
+    case ARDUINO_EVENT_ETH_CONNECTED:
+        Serial.println("ETH Connected");
+        break;
+    case ARDUINO_EVENT_ETH_GOT_IP:
+        Serial.print("ETH MAC: ");
+        Serial.print(ETH.macAddress());
+        Serial.print(", IPv4: ");
+        Serial.print(ETH.localIP());
+        if (ETH.fullDuplex())
+        {
+            Serial.print(", FULL_DUPLEX");
+        }
+        Serial.print(", ");
+        Serial.print(ETH.linkSpeed());
+        Serial.println("Mbps");
+        eth_connected = true;
+        break;
+    case ARDUINO_EVENT_ETH_DISCONNECTED:
+        Serial.println("ETH Disconnected");
+        eth_connected = false;
+        break;
+    case ARDUINO_EVENT_ETH_STOP:
+        Serial.println("ETH Stopped");
+        eth_connected = false;
+        break;
+    default:
+        break;
+    }
+
+#else
     switch (event)
     {
     case SYSTEM_EVENT_ETH_START:
@@ -138,6 +178,7 @@ void WiFiEvent(WiFiEvent_t event)
     default:
         break;
     }
+#endif
 }
 
 #endif
