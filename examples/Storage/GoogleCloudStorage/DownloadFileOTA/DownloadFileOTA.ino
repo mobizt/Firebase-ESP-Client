1f3591d5f833990e0661954848f676cdd7cf1653
suwatchai <k_suwatchai@hotmail.com>
2021-12-24 16:43:58 +0700

    Add supports (PR #219) Firmware file update over the air (OTA) via Firebase Storage and Google Cloud Storage.

--- examples/Storage/GoogleCloudStorage/DownloadFileOTA/DownloadFileOTA.ino
+++ examples/Storage/GoogleCloudStorage/DownloadFileOTA/DownloadFileOTA.ino
@@ -1,16 +1,15 @@
 /**
- * Created by K. Suwatchai (Mobizt) and WoolDoughnut310
+ * Created by K. Suwatchai (Mobizt)
  * 
  * Email: k_suwatchai@hotmail.com
  * 
  * Github: https://github.com/mobizt
- * Github: https://github.com/WoolDoughnut310
  * 
  * Copyright (c) 2021 mobizt
  *
 */
 
-//This example shows how to update firmware (bin file) from Firebase Storage bucket.
+//This example shows how to update firmware (bin file) from Google Cloud Storage bucket.
 
 #if defined(ESP32)
 #include <WiFi.h>
@@ -26,15 +25,13 @@
 #define WIFI_SSID "WIFI_AP"
 #define WIFI_PASSWORD "WIFI_PASSWORD"
 
-/* 2. Define the API Key */
-#define API_KEY "API_KEY"
-
-/* 3. Define the user Email and password that alreadey registerd or added in your project */
-#define USER_EMAIL "USER_EMAIL"
-#define USER_PASSWORD "USER_PASSWORD"
-
-/* 4. Define the Firebase storage bucket ID e.g bucket-name.appspot.com */
+/* 2. Define the Firebase storage bucket ID e.g bucket-name.appspot.com */
 #define STORAGE_BUCKET_ID "BUCKET-NAME.appspot.com"
+
+/* 3 The following Service Account credentials required for OAuth2.0 authen in Google Cloud Storage JSON API download */
+#define FIREBASE_PROJECT_ID "PROJECT_ID"
+#define FIREBASE_CLIENT_EMAIL "CLIENT_EMAIL"
+const char PRIVATE_KEY[] PROGMEM = "-----BEGIN PRIVATE KEY-----XXXXXXXXXXXX-----END PRIVATE KEY-----\n";
 
 //Define Firebase Data object
 FirebaseData fbdo;
@@ -65,19 +62,17 @@ void setup()
 
     Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
 
-    /* Assign the api key (required) */
-    config.api_key = API_KEY;
-
-    /* Assign the user sign in credentials */
-    auth.user.email = USER_EMAIL;
-    auth.user.password = USER_PASSWORD;
+    /* Assign the Service Account credentials for OAuth2.0 authen */
+    config.service_account.data.client_email = FIREBASE_CLIENT_EMAIL;
+    config.service_account.data.project_id = FIREBASE_PROJECT_ID;
+    config.service_account.data.private_key = PRIVATE_KEY;
 
     /* Assign the callback function for the long running token generation task */
     config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
 
 #if defined(ESP8266)
     //required for large file data, increase Rx size as needed.
-    fbdo.setBSSLBufferSize(1024 /* Rx buffer size in bytes from 512 - 16384 */, 1024 /* Tx buffer size in bytes from 512 - 16384 */);
+    fbdo.setBSSLBufferSize(2048 /* Rx buffer size in bytes from 512 - 16384 */, 2048 /* Tx buffer size in bytes from 512 - 16384 */);
 #endif
 
     Firebase.begin(&config, &auth);
@@ -91,7 +86,7 @@ void loop()
     {
         taskCompleted = true;
 
-        Serial.printf("Download firmware file... %s\n", Firebase.Storage.downloadOTA(&fbdo, STORAGE_BUCKET_ID /* Firebase Storage bucket id */, "<firmware.bin>" /* path of firmware file stored in the bucket */) ? "ok" : fbdo.errorReason().c_str());
+        Serial.printf("Download firmware file... %s\n", Firebase.GCStorage.downloadOTA(&fbdo, STORAGE_BUCKET_ID /* Firebase Storage bucket id */, "<firmware.bin>" /* path of firmware file stored in the bucket */) ? "ok" : fbdo.errorReason().c_str());
 
         if (fbdo.httpCode() == 200)
         {
