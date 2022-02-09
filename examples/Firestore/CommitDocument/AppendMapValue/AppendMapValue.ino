88c48d33f261d84444573748b150c4ec5286efa5
suwatchai <k_suwatchai@hotmail.com>
2022-02-09 15:36:51 +0700

    Update Firestore examples.

---
+++ examples/Firestore/CommitDocument/AppendMapValue/AppendMapValue.ino
@@ -0,0 +1,128 @@
+
+/**
+ * Created by K. Suwatchai (Mobizt)
+ *
+ * Email: k_suwatchai@hotmail.com
+ *
+ * Github: https://github.com/mobizt/Firebase-ESP-Client
+ *
+ * Copyright (c) 2022 mobizt
+ *
+ */
+
+// This example shows how to append new value to map field usng commit. This operation required Email/password, custom or OAUth2.0 authentication.
+
+#if defined(ESP32)
+#include <WiFi.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#endif
+
+#include <Firebase_ESP_Client.h>
+
+// Provide the token generation process info.
+#include <addons/TokenHelper.h>
+
+/* 1. Define the WiFi credentials */
+#define WIFI_SSID "WIFI_AP"
+#define WIFI_PASSWORD "WIFI_PASSWORD"
+
+/* 2. Define the API Key */
+#define API_KEY "API_KEY"
+
+/* 3. Define the project ID */
+#define FIREBASE_PROJECT_ID "PROJECT_ID"
+
+/* 4. Define the user Email and password that alreadey registerd or added in your project */
+#define USER_EMAIL "USER_EMAIL"
+#define USER_PASSWORD "USER_PASSWORD"
+
+// Define Firebase Data object
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long dataMillis = 0;
+int count = 0;
+
+void setup()
+{
+
+    Serial.begin(115200);
+
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print("Connecting to Wi-Fi");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(300);
+    }
+    Serial.println();
+    Serial.print("Connected with IP: ");
+    Serial.println(WiFi.localIP());
+    Serial.println();
+
+    Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+
+    /* Assign the api key (required) */
+    config.api_key = API_KEY;
+
+    /* Assign the user sign in credentials */
+    auth.user.email = USER_EMAIL;
+    auth.user.password = USER_PASSWORD;
+
+    /* Assign the callback function for the long running token generation task */
+    config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
+
+    Firebase.begin(&config, &auth);
+
+    Firebase.reconnectWiFi(true);
+}
+
+void loop()
+{
+
+    if (Firebase.ready() && (millis() - dataMillis > 60000 || dataMillis == 0))
+    {
+        dataMillis = millis();
+        count++;
+
+        Serial.print("Commit a document (append map value in document)... ");
+
+        // The dyamic array of write object fb_esp_firestore_document_write_t.
+        std::vector<struct fb_esp_firestore_document_write_t> writes;
+
+        // A write object that will be written to the document.
+        struct fb_esp_firestore_document_write_t update_write;
+
+        // Set the write object write operation type.
+        // fb_esp_firestore_document_write_type_update,
+        // fb_esp_firestore_document_write_type_delete,
+        // fb_esp_firestore_document_write_type_transform
+        update_write.type = fb_esp_firestore_document_write_type_update;
+
+        // Set the document content to write (transform)
+
+        FirebaseJson content;
+        String documentPath = "test_collection/test_document";
+
+        content.set("fields/myMap/mapValue/fields/key" + String(count) + "/stringValue", "value" + String(count));
+
+        // Set the update document content
+        update_write.update_document_content = content.raw();
+
+        update_write.update_masks = "myMap.key" + String(count);
+
+        // Set the update document path
+        update_write.update_document_path = documentPath.c_str();
+
+        // Add a write object to a write array.
+        writes.push_back(update_write);
+
+        if (Firebase.Firestore.commitDocument(&fbdo, FIREBASE_PROJECT_ID, "" /* databaseId can be (default) or empty */, writes /* dynamic array of fb_esp_firestore_document_write_t */, "" /* transaction */))
+            Serial.printf("ok\n%s\n\n", fbdo.payload().c_str());
+        else
+            Serial.println(fbdo.errorReason());
+    }
+}
