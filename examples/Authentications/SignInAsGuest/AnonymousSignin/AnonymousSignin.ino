70ae129178e142d3c2ad87e30aab66d4ab87290c
unknown <k_suwatchai@hotmail.com>
2023-01-19 20:53:12 +0700

    Bugs fix and improvements.

--- examples/Authentications/SignInAsGuest/AnonymousSignin/AnonymousSignin.ino
+++ examples/Authentications/SignInAsGuest/AnonymousSignin/AnonymousSignin.ino
@@ -98,19 +98,7 @@ void setup()
     Serial.print("Connected with IP: ");
     Serial.println(WiFi.localIP());
     Serial.println();
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-#endif
 
-    Serial.print("Connecting to Wi-Fi");
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        Serial.print(".");
-        delay(300);
-    }
-    Serial.println();
-    Serial.print("Connected with IP: ");
-    Serial.println(WiFi.localIP());
-    Serial.println();
 
     Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
 
