ebe29a5e7b1c4ccd3b96fe508c733801f22f5c8c
unknown <k_suwatchai@hotmail.com>
2023-01-20 20:32:25 +0700

    Fix wdt reset in ESP8266 core 3.1.1.

--- src/bearssl/inner.h
+++ src/bearssl/inner.h
@@ -25,13 +25,13 @@
 #ifndef INNER_H__
 #define INNER_H__
 
-#include "esp_suspend.h"
+
 #if defined(ARDUINO_NANO_RP2040_CONNECT)
 #include "bool_types.h"
 #endif
 #include <Arduino.h>
 #if defined(ESP8266) || defined(PICO_RP2040)
-
+#include "esp_suspend.h"
 #include <string.h>
 #include <limits.h>
 
