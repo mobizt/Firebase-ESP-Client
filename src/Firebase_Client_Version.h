3dbb933291c5e5622f0cb59b3848dbaef42c065f
unknown <k_suwatchai@hotmail.com>
2023-09-10 14:16:12 +0700

    Update core.

--- src/Firebase_Client_Version.h
+++
@@ -1,7 +0,0 @@
-#ifndef FIREBASE_CLIENT_VERSION
-#define FIREBASE_CLIENT_VERSION "4.3.19"
-#define FIREBASE_CLIENT_VERSION_NUM 40319
-
-/* The inconsistent file version checking to prevent mixed versions compilation. */
-#define FIREBASE_CLIENT_VERSION_CHECK(ver) (ver == FIREBASE_CLIENT_VERSION_NUM)
-#endif
